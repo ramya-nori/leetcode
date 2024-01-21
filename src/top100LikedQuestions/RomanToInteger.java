15b577fe337c5b460498205f9a01b10a8bb1fc7d
ramya-nori <ramya30988@gmail.com>
2024-01-21 20:20:03 +0530

    Update RomanToInteger.java

--- src/top100LikedQuestions/RomanToInteger.java
+++ src/top100LikedQuestions/RomanToInteger.java
@@ -1,9 +1,8 @@
 package top100LikedQuestions;
 
 /**
- *
  * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
- *
+ * <p>
  * Symbol       Value
  * I             1
  * V             5
@@ -13,41 +12,40 @@ package top100LikedQuestions;
  * D             500
  * M             1000
  * For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
- *
+ * <p>
  * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
- *
+ * <p>
  * I can be placed before V (5) and X (10) to make 4 and 9.
  * X can be placed before L (50) and C (100) to make 40 and 90.
  * C can be placed before D (500) and M (1000) to make 400 and 900.
  * Given a roman numeral, convert it to an integer.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: s = "III"
  * Output: 3
  * Explanation: III = 3.
  * Example 2:
- *
+ * <p>
  * Input: s = "LVIII"
  * Output: 58
  * Explanation: L = 50, V= 5, III = 3.
  * Example 3:
- *
+ * <p>
  * Input: s = "MCMXCIV"
  * Output: 1994
  * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
- *
- *
+ * <p>
+ * <p>
  * Constraints:
- *
+ * <p>
  * 1 <= s.length <= 15
  * s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
  * It is guaranteed that s is a valid roman numeral in the range [1, 3999].
- *
- *
  ***/
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -63,11 +61,10 @@ class RomanToInteger {
         romanValues.put('M', 1000);
         int n = s.length();
         int res = 0;
-        for (int i=0; i<n; i++) {
-            if (i<n-1 && (romanValues.get(s.charAt(i)) < romanValues.get(s.charAt(i+1)))) {
+        for (int i = 0; i < n; i++) {
+            if (i < n - 1 && (romanValues.get(s.charAt(i)) < romanValues.get(s.charAt(i + 1)))) {
                 res -= romanValues.get(s.charAt(i));
-            }
-            else {
+            } else {
                 res += romanValues.get(s.charAt(i));
             }
         }
