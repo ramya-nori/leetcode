366d57ea21f2c5312626fc4754dacbc383c3edc9
ramya-nori <ramya30988@gmail.com>
2024-01-22 22:56:51 +0530

    Update LongestCommonPrefix.java

--- src/top100LikedQuestions/LongestCommonPrefix.java
+++ src/top100LikedQuestions/LongestCommonPrefix.java
@@ -26,7 +26,7 @@ package top100LikedQuestions;
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         String str = strs[0];
-        for (int i = 0; i < strs.length; i++) {
+        for (int i = 1; i < strs.length; i++) {
             while (strs[i].indexOf(str) != 0) {
                 str = str.substring(0, str.length() - 1);
             }
